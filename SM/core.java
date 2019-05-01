d9cac440c01bc132074b909dbc06aa70bc221d8f
REA <kingsong95@naver.com>
2019-05-01 10:45:14 +0900

    Define Task Class(Task.java)
    .

---
+++ SM/core.java
@@ -0,0 +1,5 @@
+package SM;
+
+
+public class core{
+}
---
+++ Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    String taskName;
+    String taskDesc;    // Task Description. task에 대한 상세한 설명
+
+
+// 디버깅용 main 코드
+/*    public static void main(String args[]) {
+        Task a = new Task();
+        a.taskName = "hello world!";
+        System.out.println(a.taskName);
+    }*/
+}
+
+
+/*
+필요 변수들:
+date (일정 시작날짜)
+중요도 (int 1~5 정도로?)
+작성자 (이 task를 만든 사람)
+대상자 (이 일정에 해당되는 사람들. 복수일수도 있고 everyone 일수도 있음.)
+
+*/
+
