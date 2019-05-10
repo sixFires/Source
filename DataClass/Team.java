e02b80bfe26976c51db274816c04a8e9712e8170
REA <kingsong95@naver.com>
2019-05-10 13:31:13 +0900

    Getter / Setter Added. 세세한 클래스 관리는 나중에 할 것. ArrayList로 배열 들어갈 부분은 모두 교체했음.

--- DataClass/Team.java
+++ DataClass/Team.java
@@ -1,7 +1,33 @@
 package DataClass;
 
+import java.util.ArrayList;
+
 public class Team {
-    int id;
-    String team_name;
-    
+    private int id;
+    private String team_name;
+    private ArrayList<Team_Member> team_members;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return team_name;
+    }
+
+    public void setTeamName(String team_name) {
+        this.team_name = team_name;
+    }
+
+    public ArrayList<Team_Member> getTeamMembers() {
+        return team_members;
+    }
+
+    public void addTeamMembers(Team_Member team_member) {
+        this.team_members.add(team_member);
+    }
 }
--- DataClass/Team_Member.java
+++ DataClass/Team_Member.java
@@ -1,9 +1,55 @@
 package DataClass;
 
+import java.util.ArrayList;
+
 public class Team_Member {
-    int id;
-    String name;
-    String phone;
-    String email;
-    Task[] assigned_task;
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+    private ArrayList<Task> assigned_task;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        /* 정규식을 통해 번호 양식 검증하는 routine 추가 예정 */
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        /* 정규식을 통해 이메일 양식 검증하는 routine 추가 예정 */
+        this.email = email;
+    }
+
+    public ArrayList getAssignedTask() {
+        return assigned_task;
+    }
+
+    public void addTask(Task newtask) {
+        this.assigned_task.add(newtask);
+    }
+
+    /* deleteTask 등 나중에 추가 */
 }
